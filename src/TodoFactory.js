fa3839642d05c3e3c8cb0b1254b46c021a5cadbf
James Anderson <james.anderson98@outlook.com>
2024-03-29 09:50:02 +0000

    Add TodoFactory module

---
+++ src/TodoFactory.js
@@ -0,0 +1,26 @@
+import {v4 as uuidv4 } from 'uuid';
+
+export function CreateTodo (title, dueDate, priority, description/*, notes*/) {
+
+
+    // properties
+    const id = uuidv4();
+    const getTitle = () => title;
+    const getDueDate = () => dueDate;
+    const getPriority = () => priority;
+    const getDescription = () => description;
+    // const getNotes =() => notes;
+
+    // methods
+    const editTitle = (newTitle) => title = newTitle; 
+    const editDueDate = (newDueDate) => dueDate = newDueDate;
+    const editPriority = (newPriority) => priority = newPriority;
+    const editDescription = (newDescription) => description = newDescription;
+    // const editNotes = (newNotes) => notes = newNotes;
+    // const displayTodo = () => {
+    //     return { title, dueDate, priority, description/*, notes*/ };
+    // };
+
+
+    return { getTitle, getDueDate, getPriority, getDescription, /*getNotes,*/ editTitle, editDueDate, editPriority, editDescription, /*editNotes, displayTodo,*/ id };
+};
