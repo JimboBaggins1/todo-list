e39084f07a3ac829d5884a7f2dcc485a867e856e
James Anderson <james.anderson98@outlook.com>
2024-03-29 09:53:51 +0000

    Import ScreenController(). Some basic console testing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
 import { CreateProject } from './ProjectFactory.js';
 import { CreateTodo } from './TodoFactory.js';
+import { ScreenController } from './ScreenController.js';
+
+ScreenController();
+
+
+// let testProject = CreateProject('testProject');
+// console.log(testProject.getName());
+// testProject.addTodo(CreateTodo('testTodo'));
+// testProject.addTodo(CreateTodo('testTodo2'));
+
+// console.log(testProject.getProjectArray()[0].id);
+// testProject.removeTodo(testProject.getProjectArray()[0].id)
+// console.log(testProject.getProjectArray());
