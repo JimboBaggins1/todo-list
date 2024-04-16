05589af4c0567bc4d1cf12b797812d046c2dafb4
James Anderson <james.anderson98@outlook.com>
2024-04-16 15:02:40 +0100

    Add functions to check for and return stored data

--- src/StorageController.js
+++ src/StorageController.js
@@ -1,9 +1,20 @@
 export class StorageController {
-    projects = [];
+    // projects = [];
     updateProjectArray(projectArray) {
         localStorage.setItem("projects", JSON.stringify(projectArray));
-        this.projects = projectArray;
+        // this.projects = projectArray;
     }
 
+    displayStoredProjects() {
+        return JSON.parse(localStorage.getItem("projects"));
+    }
 
+    checkForStoredData() {
+        if (localStorage.length == 0) {
+            return null;
+        } else {
+            return this.displayStoredProjects();
+        }
+
+    }
 }
